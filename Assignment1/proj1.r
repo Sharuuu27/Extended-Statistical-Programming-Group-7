77ef6505171ed924b16132b8b5f6b9cf8a913aed
YuHsuan07 <yhhung0710@gmail.com>
2025-09-22 12:18:39 +0100

    Update proj1.r

--- Assignment1/proj1.r
+++ Assignment1/proj1.r
@@ -12,7 +12,8 @@ i_ob <- grep("[", a, fixed=TRUE) # locate all words in 'a' that contain '[' (not
 
 i_uob <- c() # create an empty list to store the locations of the unmatched ['
 for (i in i_ob) {
-  i_uob_temp <- grep("]", a[i:(i+100)], fixed=TRUE) # index of corresponding ']'
+  range<-min(i+100,length(a))
+  i_uob_temp <- grep("]", a[(i+1):range], fixed=TRUE) # index of corresponding ']'
   
   if (length(i_uob_temp)==0) { # if the corresponding ']' is not found within the next 100 words, 
     i_uob <- append(i_uob, i) # store the location of '[' 
