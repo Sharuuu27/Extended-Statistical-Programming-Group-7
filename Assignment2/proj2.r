f378bcfcc7fdb19d2a7a2387980906b5662fcc3f
hiroh-git <hara.yasuh@gmail.com>
2025-10-07 13:34:42 +0100

    Update proj2.r

---
+++ Assignment2/proj2.r
@@ -0,0 +1,18 @@
+# Team Members: 
+# 1. Shivasharini Govindasamy (Sharuuu27) - S2766392
+# 2. Yu-Hsuan Hung (YuHsuan07) - S2793274
+# 3. Yasuhiro Hara (hiroh-git) - S2826059
+
+# Team Contributions:
+# Yasuhiro - 
+# Yu-Hsuan - 
+# Shivasharini - 
+
+## Code to [...].
+## The challenge is that [...].
+## 3 strategies are suggested:
+## 1. [...].
+## 2. [...].
+## 3. [...].
+## [...]. 
+
