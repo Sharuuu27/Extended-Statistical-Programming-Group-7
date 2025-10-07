9a8dee88b3e65f3f01773ae9331f0d3b2bac4918
Shivasharini <s2766392@ed.ac.uk>
2025-10-07 22:15:42 +0100

    Question 1

--- Assignment2/proj2.r
+++ Assignment2/proj2.r
@@ -16,3 +16,15 @@
 ## 3. [...].
 ## [...]. 
 
+## Question 1
+# To assign the n people to thier unique household ID
+household_id <- function(n = 1000, hmax = 5) {
+  # n = total population
+  # hmax = maximum members in a household
+  h <- rep(1:n, times = sample(1:hmax, n, replace = TRUE))
+  # the size of households are uniformly distributed
+  return(h[1:n])
+  # to make sure the population = n
+}
+
+
