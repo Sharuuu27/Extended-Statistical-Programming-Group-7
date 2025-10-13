83bc05481becbc09976f1234e8230fcd59282c3b
hiroh-git <hara.yasuh@gmail.com>
2025-10-13 14:09:01 +0100

    Update proj2.r

--- Assignment2/proj2.r
+++ Assignment2/proj2.r
@@ -83,7 +83,7 @@ nseir <- function(beta, h, alink, alpha = c(.1, .01, .01), delta = .2,
   
   ##Initialization 
   set.seed(0)
-  n <- length(beta) 
+  n <- length(beta)
   x <- rep(0, n)
   ni <- round(n * pinf)
   
@@ -111,40 +111,40 @@ nseir <- function(beta, h, alink, alpha = c(.1, .01, .01), delta = .2,
     
     # S -> E (Three ways)
     for (i in I_idx) {  ## Loop through each infector i
-      h_id <- h[i]      ## get the household ID of j
-      net <- alink[[i]] ## get the regular network contacts (indices) of j
+      h_id <- h[i]      ## get the household ID of i
+      net <- alink[[i]] ## get the regular network contacts (indices) of i
       
-      ## 1st way: household transmission 
-      ## find people who are S(0) and in j's household
+      ## 1st way: household transmission
+      ## find people who are S(0) and in i's household
       S_in_hh_idx <- which(x == 0 & h == h_id)
       ## check if there are any S targets in the household
       if (length(S_in_hh_idx) > 0) {
         ## generate uniform random deviates
-        u_hh <- runif(length(S_in_hh_idx)) 
+        u_hh <- runif(length(S_in_hh_idx))
         ## determine who is infected (prob alpha[1]).
         x[S_in_hh_idx[u_hh < alpha[1]]] <- 1 ## S-> E with prob alpha[1]
       }
       
-      ## 2nd way: Regular Network Transmission 
+      ## 2nd way: Regular Network Transmission
       ## Filter out contacts who are still S(0)
-      S_net_idx <- net[x[net] == 0] 
+      S_net_idx <- net[x[net] == 0]
       ### check if there are any S targets in the regular network
       if (length(S_net_idx) > 0) {
         ## generate uniform random deviates
-        u_net <- runif(length(S_net_idx)) 
+        u_net <- runif(length(S_net_idx))
         ## determine who is infected (prob alpha[2])
         x[S_net_idx[u_net < alpha[2]]] <- 1 ## S-> E with prob alpha[2]
       }
       
-      ## 3rd way: Irrespective Transmission 
+      ## 3rd way: Irrespective Transmission
       S_all_idx <- which(x == 0)
       ## check if there are any S targets
       if (length(S_all_idx) > 0) {
-        ##Setting P_random
-        ##Use pmin to cap probability at 1
+        ## Setting P_random
+        ## Use pmin to cap probability at 1
         P_random <- pmin(alpha[3] * nc * beta[i] * beta[S_all_idx] / prob_deno, 1)
-        ### generate uniform random deviates
-        u_random <- runif(length(S_all_idx)) 
+        ## generate uniform random deviates
+        u_random <- runif(length(S_all_idx))
         ## determine who is infected (prob P_random)
         x[S_all_idx[u_random < P_random]] <- 1 ## S-> E with prob P_random
       }
@@ -162,40 +162,36 @@ nseir <- function(beta, h, alink, alpha = c(.1, .01, .01), delta = .2,
 ## Question4
 ## A function to nicely plot the dynamics of the simulated population states
 plot_graphs <- function(alphas, betas, labels) {
-  ## alphas = the daily probs I[j] -> S[i] in matrix format;
+  ## alphas = the daily probs I[i] -> S[j] in matrix format;
   ## betas = the transmission rate parameter in matrix format;
   ## labels = title of each parameter settings
   
   ## setting for plotting multiple graphs
   ncol <- length(alphas[1,])    ## set number of graphs to plot
-  par(mfcol=c(2,ncol),          ## layout setting of the plot
+  par(mfcol=c(2,ncol/2),        ## layout setting of the plot
       mar=c(4,4,2,1),           ## margin settings
       oma=c(.2,.2,.2,.2),       ## outer margin settings
-      family="serif",           ## font style settings
+      family="sans",            ## font style settings (Arial)
       cex.axis=.6, cex.lab=.9)  ## font size settings
   
   for (i in 1:ncol){
     ## draw y axis labels only on the first graph
-    if (i==1) {
-      histY <- "Frequency"; scY <- "N" 
-    } else {
-      histY <- ""; scY <- ""
-    }
+    if (i<=2) scY <- "N" else scY <- ""      ## draw Y-axis label on the left graphs
+    if (i%%2==0) scX <- "day" else scX <- "" ## draw X-asix label on the bottom graphs
     
     ## Simulate SEIR model with nseir function
     epi <- nseir(beta=betas[,i],h,alink,alpha=alphas[,i])
     
-    ## plot histogram (beta distribution)
-    hist(betas[,i],xlab="beta",ylab=histY,main=labels[i], cex.main=0.8,
-         breaks=seq(0,1,by=.1),col="skyblue", border="darkblue")
-    
     ## plot simulated data
-    plot(epi$S,ylim=c(0,max(epi$S)),xlab="day",ylab=scY,
-         panel.first=grid(5,5),type="o",cex=.5) ## S (black)
-    lines(epi$E,col=4,type="o",cex=.5)          ## E (blue)
-    lines(epi$I,col=2,type="o",cex=.5)          ## I (red)
-    legend("topright", legend=c("S", "E", "I"), ## legend
-           col=c(1, 4, 2), pch=1, lty=1, bty="l",cex=.8)
+    plot(epi$S,ylim=c(0,max(epi$S)),xlab=scX,ylab=scY,
+         main=labels[i],
+         type="l",cex=.5,cex.main=.8,lwd=4)     ## S (black)
+    lines(epi$E,col=4,type="l",cex=.5,lwd=4)    ## E (blue)
+    lines(epi$I,col=2,type="l",cex=.5,lwd=4)    ## I (red)
+    lines(epi$R,col=3,type="l",cex=.5,lwd=4)    ## I (red)
+    legend(x="right",y="center",                ## legend
+           legend=c("S", "E", "I", "R"), col=c(1, 4, 2, 3),
+           lty=1, bty="n",cex=.7,lwd=3)
   }
 }
 
@@ -215,9 +211,10 @@ beta_mean <- rep(mean(beta_u),n)
 betas <- array(c(beta_u,beta_u,beta_mean,beta_mean),
                dim=c(n,4))
 
-labels <- c("\nDefault parameters",
-            "Without household and\n regular network",
-            "\nConstant β",
-            "Combination\n(constant β, random mixing)")
+labels <- c("Default parameters\n(αh=.1,αc=αr=.01)",
+            "Without household and regular network\n(αh=αc=0,αr=.04)",
+            "Constant β\n(αh=.1,αc=αr=.01)",
+            "Constant β & random mixing\n(αh=αc=0,αr=.04)")
 
+## Plot graphs
 plot_graphs(alphas, betas, labels)
