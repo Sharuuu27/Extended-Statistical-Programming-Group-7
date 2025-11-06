53e7aaeff7bae66427fe2d09af68e66f125edf91
hiroh-git <hara.yasuh@gmail.com>
2025-11-06 21:07:01 +0000

    Update proj4.R

--- Assignment4/proj4.R
+++ Assignment4/proj4.R
@@ -24,13 +24,13 @@
 
 library(splines)
 #setwd("file")
-data <- read.table("engcov.txt",header = TRUE)
-data$date <- as.Date(data$date, format = "%d/%m/%y")
+data <- read.table("engcov.txt", header=TRUE)
+data$date <- as.Date(data$date, format="%d/%m/%y")
 
 
 ## Question1
 
-matrixes <- function(data, k = 80){
+matrixes <- function(data, k=80){
 # define a function to compute X_tilde, X and S 
   n <- nrow(data) # number of observation days, n
   t1 <- data$julian[1] - 30  # earliest infection day, setting as t1-30
@@ -38,14 +38,14 @@ matrixes <- function(data, k = 80){
   t_coverage <- t1:tn        # the scope of infection days
   # k-2 knots cover the scope of infection days
   unit <- (tn-t1)/(k-2-1) 
-  knot <- seq(t1-unit*3, tn+unit*3,length.out=(k+4))
+  knot <- seq(t1-unit*3, tn+unit*3, length.out=(k+4))
   # adding 6 points beyond the original k+2 knots, ensuring a total of k+4 knots
-  x_tilde <- splineDesign(knots=knot,x=t_coverage,ord=4,outer.ok=TRUE)
+  x_tilde <- splineDesign(knots=knot, x=t_coverage, ord=4, outer.ok=TRUE)
   # design matrix, X_tilde, for the B-splines defined by knots at the values 
   # in the scope of infection days
   s <- crossprod(diff(diag(k), diff=2)) 
   # smoothing penalty matrix S
-  x <- matrix(0, nrow = n, ncol = k)
+  x <- matrix(0, nrow=n, ncol=k)
   # initialise the death model matrix X
   d <- 1:80 # the range of days from infection to death.
   edur <- 3.151 # mean parameter for the log-normal distribution.
@@ -53,15 +53,15 @@ matrixes <- function(data, k = 80){
   pd <- dlnorm(d, edur, sdur) # probability density
   pd <- pd / sum(pd) # normalize probabilities pd
   for (i in 1:n) {
-    j_max <- min(29 + i, 80) 
+    j_max <- min(29+i, 80) 
     # determine the maximum interval j_max for the convolution limit.
     for (j in 1:j_max) {
       y <- 30+i-j 
-      x[i,] <- x[i,]+x_tilde[y,]*pd[j]
+      x[i,] <- x[i,] + x_tilde[y,]*pd[j]
       # X_{i,k}=\sum_{j=1}^{min(29+i,80)}\tilde{X}_{(30+i-j),k}\times \pi_{j}
     }
   }
-  list(X_tilde = x_tilde, X = x, S = s) # return the three computed matrices
+  list(X_tilde=x_tilde, X=x, S=s) # return the three computed matrices
 }
 
 # extract x_tilde, x, and s 
@@ -78,8 +78,8 @@ pnll <- function(gamma,x,y,s,lambda){
 # define the penalised negative log likelihood (pnll) function
   beta <- exp(gamma) # beta = exp(gamma), ensuring positive f
   mu <- x %*% beta   # mu = X %*% beta
-  ll <- y*log(mu)-mu-lgamma(y+1) # log-likelihood
-  p <- lambda*(t(beta)%*%s%*%beta)/2 # penalty
+  ll <- y*log(mu) - mu - lgamma(y+1) # log-likelihood
+  p <- lambda*(t(beta) %*% s %*% beta)/2 # penalty
   -sum(ll) + p # return pnll= -sum(ll) + p
 }
 
@@ -97,21 +97,21 @@ gpnll <- function(gamma,x,y,s,lambda){
 k <- 80
 fd <- gamma0 <- rep(log(1e-3),k) # set gamma0 for testing
 lambda0 <- 5e-5 # set test lambda value.
-pnll0 <- pnll(gamma0,lambda0,x=x,y=y,s=s) # pnll at gamma0 and lambda0
+pnll0 <- pnll(gamma0, lambda0, x=x, y=y, s=s) # pnll at gamma0 and lambda0
 eps <- 1e-7 # finite difference interval 
 for (i in seq_along(gamma0)) { # loop over gamma0
   gamma1 <- gamma0
   gamma1[i] <- gamma1[i] + eps  # increase gamma1[i] by eps         
-  pnll1 <- pnll(gamma1, lambda0, x = x, y = y, s = s) # compute resulting pnll
+  pnll1 <- pnll(gamma1, lambda0, x=x, y=y, s=s) # compute resulting pnll
   fd[i] <- (pnll1 - pnll0) / eps   # approximate -dl/dgamma[i]      
 }
-head(fd);head(gpnll(gamma0,lambda0,x=x,y=y,s=s)) 
+head(fd);head(gpnll(gamma0, lambda0, x=x, y=y, s=s)) 
 # the result indicating that gpnll is coded correctly
 
 
 ## Question3
-fit <- optim(par=gamma0,fn=pnll,gr=gpnll,
-             lambda=5e-5,x=x,y=y,s=s,
+fit <- optim(par=gamma0, fn=pnll, gr=gpnll,
+             lambda=5e-5, x=x, y=y, s=s,
              method="BFGS") #, hessian=TRUE
 # Use optim function with BFGS method to optimise
 gamma_h <- fit$par       # Get the fitted optimal parameters gamma_h
@@ -145,8 +145,8 @@ legend("topright",
 # Defining the grid
 lsp <- seq(-13, -7, length=50)
 lambdas <- exp(lsp)
-#n <- length(y) # number of observation
-#k <- nrow(data) # number of parameters: 80
+#n <- nrow(data) # number of observation
+#k <- ncol(x) # number of parameters: 80
 
 BIC <- EDF <- rep(0, length(lsp))
 
